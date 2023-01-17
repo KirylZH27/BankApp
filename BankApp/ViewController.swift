93d4eab8fa345ccb2311519621f6046eaf403c7f
KirylZH27 <122560591+KirylZH27@users.noreply.github.com>
2023-01-17 19:14:19 +0300

    initProject

---
+++ BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Кирилл Жогальский on 17.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
