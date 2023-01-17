7c3e6133e6fa539a06e178831a0616ba0ab24320
KirylZH27 <122560591+KirylZH27@users.noreply.github.com>
2023-01-17 20:12:57 +0300

    createdTabBarWithProfileVc

---
+++ BankApp/ProfileController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileController.swift
+//  BankApp
+//
+//  Created by Кирилл Жогальский on 17.01.23.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
